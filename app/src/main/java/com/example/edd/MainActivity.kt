59361ff08f8c9dfd36006081cc67ed7355820dbb
DarkSerc <fnd03@mail.ru>
2023-06-13 19:18:00 +0300

    19:17

--- app/src/main/java/com/example/edd/MainActivity.kt
+++ app/src/main/java/com/example/edd/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.edd
 
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import com.example.edd.databinding.ActivityMainBinding
-
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
