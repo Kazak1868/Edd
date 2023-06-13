ecdeb5479edf48fd709284d13089fae12bf5622a
Kazak1868 <127226501+Kazak1868@users.noreply.github.com>
2023-06-13 19:10:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/edd/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.edd
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import com.example.edd.databinding.ActivityMainBinding
+
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
