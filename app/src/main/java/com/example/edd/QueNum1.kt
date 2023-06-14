5f6f2b2d30d02f4d620739620b384ea32eeae2af
DarkSerc <fnd03@mail.ru>
2023-06-14 03:54:55 +0300

    s

--- app/src/main/java/com/example/edd/QueNum1.kt
+++ app/src/main/java/com/example/edd/QueNum1.kt
@@ -3,12 +3,15 @@ package com.example.edd
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 import com.example.edd.databinding.ActivityQueNum1Binding
 
 class QueNum1 : AppCompatActivity() {
     private lateinit var binding: ActivityQueNum1Binding
+var boolean Colory = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQueNum1Binding.inflate(layoutInflater)
@@ -18,11 +21,12 @@ class QueNum1 : AppCompatActivity() {
             var tehh = binding.answer1.text.toString()
             if(tehh == "с")
             {
-                public Q1 bool = 0
                 val perehod = Intent(this, MainActivity3::class.java)
                 startActivity(perehod)
-                Toast.makeText(this,"@string/Gotovo", Toast.LENGTH_LONG).show()
+                Toast.makeText(this,"Готово", Toast.LENGTH_LONG).show()
+                Colory = 1
             }
+
         }
     }
 }
